36344988e332ca67a3672dbf8b1cf1f910e76c90
Renato Pablo <renatocuim@gmail.com>
2025-03-11 11:12:32 -0300

    LIST

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//CARREGA UM USUARIO 
+// $root = new Usuario();
+// $root->loadById(4); 
+//echo $root;
 
-$root->loadById(4);
- 
-echo $root;
+//CARREGA UMA LISTA DE USUARIOS
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+//CARREGA UMA LISTA DE USUARIO BUSCANDO PELO LOGIN
+// $search = Usuario::search("Camila");
+// echo json_encode($search);
+
+//CARREGA UM USUARIO USANDO LOGIN E SENHA
+$usuario = new Usuario();
+$usuario->login("Camila", "123456");
+
+echo $usuario;
 
 ?>
